4c233df7487c2fc7fa3f29647baf2f23393e5a72
Richard Allison <richard.allison.18@outlook.com>
2018-02-21 20:14:21 +0000

    Created JavaScript api request

--- public/app.js
+++ public/app.js
@@ -1,5 +1,20 @@
+// -------------------
 const app = function () {
-
+  const url = "https://api.punkapi.com/v2/beers"
+  makeRequest(url, requestComplete);
 };
+// -------------------
+
+const makeRequest = function(url, callback) {
+  const request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.addEventListener("load", callback);
+  request.send();
+}
+
+const requestComplete = function(){
+  if(this.status !== 200) return;
+  const jsonString = this.responseText;
+}
 
 document.addEventListener('DOMContentLoaded', app);
