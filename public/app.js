3055905a1159f5b788d0b9b734627204dc1259d7
Richard Allison <richard.allison.18@outlook.com>
2018-02-21 20:36:28 +0000

    Refactored code into smaller reusable functions

--- public/app.js
+++ public/app.js
@@ -5,28 +5,36 @@ const app = function () {
 };
 // -------------------
 
-const makeRequest = function(url, callback) {
+const makeRequest = function (url, callback) {
   const request = new XMLHttpRequest();
   request.open("GET", url);
   request.addEventListener("load", callback);
   request.send();
-}
+};
 
-const requestComplete = function(){
+const requestComplete = function () {
   if(this.status !== 200) return;
   const jsonString = this.responseText;
   const beers = JSON.parse(jsonString);
   populateList(beers);
-}
+};
 
-const populateList = function(beers){
+const createListItem = function (beer) {
+  const li = document.createElement('li');
+  li.innerText = beer.name;
+  return li;
+};
+
+const appendListItem = function (li) {
   const ul = document.getElementById("beer-list");
+  ul.appendChild(li);
+};
 
-  beers.forEach(function(beer){
-   const li = document.createElement("li");
-   li.innerText = beer.name;
-   ul.appendChild(li);
-  })
-}
+const populateList = function (beers) {
+  beers.forEach(function (beer) {
+   const li = createListItem(beer);
+   appendListItem(li);
+ });
+};
 
 document.addEventListener('DOMContentLoaded', app);
